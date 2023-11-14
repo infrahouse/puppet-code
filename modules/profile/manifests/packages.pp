8183414229293e97869fe6518af5dece3b6d5446
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-11-14 07:51:32 -0800

    Add more packages to jumphost
        * 'make',
        * 'python3',
        * 'python-is-python3',
        * 'python3-virtualenv'

--- modules/profile/manifests/packages.pp
+++ modules/profile/manifests/packages.pp
@@ -4,7 +4,11 @@ class profile::packages () {
   package { [
     'awscli',
     'jq',
-    'net-tools'
+    'make',
+    'net-tools',
+    'python3',
+    'python-is-python3',
+    'python3-virtualenv'
   ]:
     ensure => present
   }
