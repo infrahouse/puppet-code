fff8d3a8a8efbcd89eb4c25aef8700766f90dee0
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-02-08 14:24:25 -0800

    Pass elasticrserach cluster name tp config

--- modules/profile/manifests/elastic/config.pp
+++ modules/profile/manifests/elastic/config.pp
@@ -1,9 +1,11 @@
 # @summary: Installs elasicsearch service.
 class profile::elastic::config (
-  String $role = 'master'
+  String $role = 'master',
+  String $name = 'elasticsearch',
 ) {
 
   $elastic_cluster_role = $role
+  $elastic_cluster_name = $name
   file { '/etc/elasticsearch/elasticsearch.yml':
     ensure  => file,
     content => template('profile/elasticsearch.yml.erb'),
