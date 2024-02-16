7d47f4da915a1c10a0143c7fd480e8b0df7eae30
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-02-16 13:39:03 -0800

    Sync kibana_system password with AWS secretsmanager secret.
    The secret it read from AWS secret stored in the puppet fact `$facts['elasticsearch']['kibana_system_secret']`.

--- modules/profile/manifests/elastic_data.pp
+++ modules/profile/manifests/elastic_data.pp
@@ -4,6 +4,7 @@ class profile::elastic_data () {
   include 'profile::elastic::repo'
   include 'profile::elastic::packages'
   include 'profile::elastic::service'
+  include 'profile::elastic::kibana_user'
 
   class { 'profile::elastic::config':
     role         => 'data',
