9212d1fa444efef88672e2c4ff106ef501da7819
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-02-20 11:44:21 -0800

    Fix the backup command

--- modules/profile/manifests/elastic/backups.pp
+++ modules/profile/manifests/elastic/backups.pp
@@ -2,7 +2,7 @@
 class profile::elastic::backups () {
 
   cron { 'elastic-backup':
-    command => '/usr/local/bin/ih-elastic snapshots status backups',
+    command => '/usr/local/bin/ih-elastic snapshots create backups',
     user    => 'root',
     hour    => fqdn_rand(24),
     minute  => fqdn_rand(60),
