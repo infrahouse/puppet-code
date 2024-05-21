b575a619fae508f5d049a04da7b6541e1e6792e0
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-05-20 18:18:13 -0700

    Initial bookstack configuration (#69)
    The app itself, the database

--- modules/profile/manifests/bookstack.pp
+++ modules/profile/manifests/bookstack.pp
@@ -1,6 +1,41 @@
 # @summary: Installs bookstack applicartion
-class profile::bookstack () {
+class profile::bookstack (
+  $bookstack_package_url = lookup(
+    'profile::bookstack::bookstack_package_url',
+    undef,
+    undef,
+    'https://github.com/BookStackApp/BookStack/archive/refs/tags/v24.05.tar.gz'
+  )
+)
+{
   include 'profile::base'
-  include 'profile::bookstack::packages'
+
+  $bookstack_root = '/var/www/bookstack'
+  $www_user = 'www-data'
+  $www_group = 'www-data'
+
+  class { 'profile::bookstack::packages':
+    bookstack_package_url => $bookstack_package_url,
+    bookstack_root        => $bookstack_root,
+    www_user              => $www_user,
+    www_group             => $www_group,
+  }
+
+  $dns_name = $facts['efs']['dns_name']
+  $nfs_device = "${dns_name}:/"
+  class { 'profile::bookstack::volume':
+    bookstack_root => $bookstack_root,
+    nfs_device     => $nfs_device,
+    mount_target   => "${bookstack_root}/public/uploads",
+    www_user       => $www_user,
+    www_group      => $www_group,
+  }
+
+  class { 'profile::bookstack::config':
+    bookstack_root => $bookstack_root,
+    www_user       => $www_user,
+    www_group      => $www_group,
+  }
+
   include 'profile::bookstack::service'
 }
