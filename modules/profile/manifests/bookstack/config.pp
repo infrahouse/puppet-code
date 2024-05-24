e8cfc4b3d50a7aeb70eb26d435155fddfbc488b1
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-05-23 17:21:13 -0700

    Configure SMTP and Google OAuth for bookstack (#71)

--- modules/profile/manifests/bookstack/config.pp
+++ modules/profile/manifests/bookstack/config.pp
@@ -14,6 +14,26 @@ class profile::bookstack::config (
     $facts['bookstack']['db_password_secret'], $facts['ec2_metadata']['placement']['region']
   )['password']
 
+  $bookstack_mail_from_name = $facts['bookstack']['mail_from_name']
+  $bookstack_mail_from = $facts['bookstack']['mail_from']
+
+  $bookstack_mail_host = $facts['bookstack']['mail_host']
+  $bookstack_mail_port = $facts['bookstack']['mail_port']
+  $bookstack_mail_username = $facts['bookstack']['mail_username']
+  $bookstack_mail_password = aws_get_secret(
+    $facts['bookstack']['mail_password_secret'],
+    $facts['ec2_metadata']['placement']['region']
+  )
+  $bookstack_mail_encryption = $facts['bookstack']['mail_encryption']
+  $bookstack_mail_verify_ssl = $facts['bookstack']['mail_verify_ssl']
+
+  $bookstack_google_app_id = aws_get_secret(
+    $facts['bookstack']['google_oauth_client_secret'], $facts['ec2_metadata']['placement']['region']
+  )['web']['client_id']
+  $bookstack_google_app_secret = aws_get_secret(
+    $facts['bookstack']['google_oauth_client_secret'], $facts['ec2_metadata']['placement']['region']
+  )['web']['client_secret']
+
   file { "${bookstack_root}/.env":
     ensure  => file,
     owner   => $www_user,
