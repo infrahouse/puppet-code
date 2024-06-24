9297ac8eccc891a0b1119cd39c5e2102e7500e0e
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-06-24 13:33:39 -0700

    Make elastic backup cron job quiet (#86)

--- modules/profile/manifests/elastic/backups.pp
+++ modules/profile/manifests/elastic/backups.pp
@@ -2,7 +2,7 @@
 class profile::elastic::backups () {
 
   cron { 'elastic-backup':
-    command => '/usr/local/bin/ih-elastic snapshots create backups',
+    command => '/usr/local/bin/ih-elastic --quiet snapshots create backups',
     user    => 'root',
     hour    => fqdn_rand(24),
     minute  => fqdn_rand(60),
