fd3458e2fd2af38b720fb435d2c1b0e4da47be82
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-06-29 06:43:42 -0700

    [draft]: openvpn-server role (#91)

--- modules/role/manifests/openvpn_server.pp
+++ modules/role/manifests/openvpn_server.pp
@@ -1,7 +1,9 @@
-# @summary: Puppet role for a ECS node
+# @summary: Puppet role for an OpenVPN server
 class role::openvpn_server () {
 
   include 'profile::base'
+  include 'profile::openvpn_server'
+
   class { 'profile::postfix':
     postfix_inet_interfaces => '127.0.0.1',
   }
