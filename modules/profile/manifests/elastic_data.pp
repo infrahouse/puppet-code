0d3c4552c8f491a5f85b0a4e24c35721e4b4e6f5
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-02-03 10:01:43 -0800

    Remove elastic repo
    The elastic repo is broken after it moved to Artifactory
    ```
    E: Repository 'https://artifacts.elastic.co/packages/8.x/apt stable InRelease' changed its 'Origin' value from 'elastic' to 'Artifactory'
    E: Repository 'https://artifacts.elastic.co/packages/8.x/apt stable InRelease' changed its 'Label' value from '. stable' to 'Artifactory'
    N: This must be accepted explicitly before updates for this repository can be applied. See apt-secure(8) manpage for details.
    ```
    Instead, pull elasticsearch packages from InfraHouse repo.

--- modules/profile/manifests/elastic_data.pp
+++ modules/profile/manifests/elastic_data.pp
@@ -1,7 +1,6 @@
 # @summary: Elasticsearch data node.
 class profile::elastic_data () {
 
-  include 'profile::elastic::repo'
   include 'profile::elastic::packages'
   include 'profile::elastic::service'
   include 'profile::elastic::kibana_user'
