855923fc5146284926094e29832b447af8940a8e
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-02-15 08:49:38 -0800

    Sync global to development and sandbox (#143)
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

--- environments/sandbox/modules/profile/manifests/elastic_master.pp
+++ environments/sandbox/modules/profile/manifests/elastic_master.pp
@@ -1,7 +1,6 @@
 # @summary: Elasticsearch master node.
 class profile::elastic_master () {
 
-  include 'profile::elastic::repo'
   include 'profile::elastic::packages'
   include 'profile::elastic::service'
   include 'profile::elastic::prometheus'
