8dccc1ed65401340f8b8c59d1c55a071a5547216
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-03-10 10:31:55 -0700

    Promote elastic profile to global modules (#152)

--- modules/profile/manifests/elastic/config.pp
+++ modules/profile/manifests/elastic/config.pp
@@ -10,67 +10,10 @@ class profile::elastic::config (
   $le_domain = $facts['letsencrypt']['domain']
   $le_fqdn = "${hostname}.${le_domain}"
 
-  file { '/etc/elasticsearch/letsencrypt':
-    ensure => directory,
-    owner  => 'elasticsearch',
-    mode   => '0700',
-  }
-
-  file { '/etc/elasticsearch/letsencrypt/privkey.pem':
-    ensure  => present,
-    owner   => 'elasticsearch',
-    mode    => '0600',
-    source  => "/etc/letsencrypt/live/${le_fqdn}/privkey.pem",
-    links   => follow,
-    require => [
-      Exec['obtain_certificate'],
-      File['/etc/elasticsearch/letsencrypt'],
-    ]
-  }
-
-  file { '/etc/elasticsearch/letsencrypt/cert.pem':
-    ensure  => present,
-    owner   => 'elasticsearch',
-    mode    => '0600',
-    source  => "/etc/letsencrypt/live/${le_fqdn}/cert.pem",
-    links   => follow,
-    require => [
-      Exec['obtain_certificate'],
-      File['/etc/elasticsearch/letsencrypt'],
-    ]
-  }
-
-  file { '/etc/elasticsearch/letsencrypt/fullchain.pem':
-    ensure  => present,
-    owner   => 'elasticsearch',
-    mode    => '0600',
-    source  => "/etc/letsencrypt/live/${le_fqdn}/fullchain.pem",
-    links   => follow,
-    require => [
-      Exec['obtain_certificate'],
-      File['/etc/elasticsearch/letsencrypt'],
-    ]
-  }
-
-  file { '/etc/elasticsearch/letsencrypt/chain.pem':
-    ensure  => present,
-    owner   => 'elasticsearch',
-    mode    => '0600',
-    source  => "/etc/letsencrypt/live/${le_fqdn}/chain.pem",
-    links   => follow,
-    require => [
-      Exec['obtain_certificate'],
-      File['/etc/elasticsearch/letsencrypt'],
-    ]
-  }
-
   file { '/etc/elasticsearch/elasticsearch.yml':
     ensure  => file,
     content => template('profile/elasticsearch.yml.erb'),
     notify  => Service['elasticsearch'],
-    require => [
-      Exec['obtain_certificate']
-    ],
   }
 
   file { '/etc/elasticsearch/roles.yml':
