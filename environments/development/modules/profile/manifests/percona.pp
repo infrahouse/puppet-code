dbbfd52b6c45775b2eb7fd45c3407483e8dfe745
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-03-01 08:17:34 -0800

    Support Percona Server 8.4 controlled by server_version fact (development)
    Derive Percona series (8.0/8.4), repo name, package version, and
    xtrabackup package from $facts['percona']['server_version']. Shared
    logic lives in profile::percona, subclasses reference via scoped vars.
    
    - repo.pp: idempotent percona-release setup with unless guards;
      enable pxb-84-lts repo for xtrabackup-84
    - packages.pp: support version pinning with auto-normalization of
      short versions (8.4.7-7 → 8.4.7-7-1.noble)
    - config.pp/mysqld.cnf.erb: omit deprecated binlog_format and
      log_slave_updates on 8.4

--- environments/development/modules/profile/manifests/percona.pp
+++ environments/development/modules/profile/manifests/percona.pp
@@ -1,6 +1,39 @@
 # @summary: Installs Percona Server and related components
 class profile::percona () {
 
+  $server_version = pick_default($facts.dig('percona', 'server_version'), '')
+
+  $percona_series = $server_version ? {
+    ''        => '8.0',
+    'latest'  => '8.4',
+    /^8\.4\./ => '8.4',
+    default   => '8.0',
+  }
+
+  $repo_name = $percona_series ? {
+    '8.4'   => 'ps-84-lts',
+    default => 'ps80',
+  }
+
+  # Normalize version to full apt format
+  # Percona apt versions have a distro suffix, e.g. 8.4.7-7-1.noble
+  # Accept both short (8.4.7-7) and full (8.4.7-7-1.noble) forms
+  $codename = $facts['os']['distro']['codename']
+  $server_ensure = $server_version ? {
+    ''       => 'installed',
+    'latest' => 'installed',
+    default  => $server_version =~ /\.${codename}$/ ? {
+      true    => $server_version,
+      default => "${server_version}-1.${codename}",
+    },
+  }
+
+  # XtraBackup package name differs between series
+  $xtrabackup_package = $percona_series ? {
+    '8.4'   => 'percona-xtrabackup-84',
+    default => 'percona-xtrabackup-80',
+  }
+
   include 'profile::percona::repo'
   include 'profile::percona::packages'
   include 'profile::percona::config'
