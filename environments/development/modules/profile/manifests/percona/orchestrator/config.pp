f7acc93e9c7b1c68628bd0f7d03d364cfb17ff45
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2026-03-01 19:05:24 -0800

    Add a post failover hook to Orchestrator (#252)
    When Orchestrator performs a failover, a number of tasks need to be
    completed to adjust to a new topology
    
    * We need ad add a new master to the writer target groups and remove the
      old
    * We need to protect the new master from a scale-in events
    * We need to make the new master read-only (super read only, actually)
    
    ORCHESTRATOR_INSTANCE is added to environment variables so commands like
    `orchestrator-client -c topology` work without specifying
    `-i ip-10-1-1-1`

--- environments/development/modules/profile/manifests/percona/orchestrator/config.pp
+++ environments/development/modules/profile/manifests/percona/orchestrator/config.pp
@@ -3,8 +3,13 @@ class profile::percona::orchestrator::config () {
   $orchestrator_password = aws_get_secret(
     $facts['percona']['credentials_secret'], $facts['ec2_metadata']['placement']['region']
   )['orchestrator']
-  $cluster_id = $facts['percona']['cluster_id']
-  $private_ip = $facts['networking']['ip']
+  $cluster_id         = $facts['percona']['cluster_id']
+  $dynamodb_table     = $facts['percona']['dynamodb_table']
+  $credentials_secret = $facts['percona']['credentials_secret']
+  $vpc_cidr           = $facts['percona']['vpc_cidr']
+  $read_tg_arn        = $facts['percona']['read_tg_arn']
+  $write_tg_arn       = $facts['percona']['write_tg_arn']
+  $private_ip         = $facts['networking']['ip']
 
   # Discover Raft nodes by querying the ASG at compile time
   $raft_nodes_raw = generate('/usr/local/bin/ih-ec2', 'list', "--cluster_id=${cluster_id}", '-c')
@@ -19,8 +24,16 @@ class profile::percona::orchestrator::config () {
     require => File['/var/lib/orchestrator'],
   }
 
+  $networking_hostname = $facts['networking']['hostname']
+
+  file { '/etc/profile.d/orchestrator.sh':
+    ensure  => file,
+    content => template('profile/percona/orchestrator-profile.sh.erb'),
+  }
+
   file { '/etc/orchestrator.conf.json':
     ensure  => file,
+    mode    => '0600',
     content => template('profile/percona/orchestrator.conf.json.erb'),
     notify  => Service['orchestrator'],
     require => Package['orchestrator'],
